7c22ab1900c4bc120d3333b2a3b7c1bf0ea7d25d
Axel Enghamre <axel@enghamre.se>
2022-11-29 16:30:51 +0100

    Added popup but it needs some more work

---
+++ scripts/popup.js
@@ -0,0 +1,25 @@
+const popup = document.querySelector('#popup');
+const exitPopup = popup.querySelector('div img');
+
+const products = document.querySelector('#carousel');
+
+let popupHasBeenShown = false
+
+window.addEventListener('scroll',()=>{
+    const buttomOfScreen = parseInt(window.scrollY+window.innerHeight);
+
+    const productsFromTop = parseInt(products.offsetTop - products.scrollTop + products.clientTop);
+
+
+    if(buttomOfScreen == productsFromTop && !popupHasBeenShown) {
+        popupHasBeenShown = true
+        popup.classList.add('grid');
+        popup.classList.remove('hidden');
+    }
+});
+
+exitPopup.addEventListener('click', ()=>{
+    popup.classList.remove('grid');
+    popup.classList.add('hidden');
+});
+
