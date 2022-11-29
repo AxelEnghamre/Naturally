fdc77a61fe652d04980f362175f26f1c297888f6
sirisayshello <veckovis.salongen0p@icloud.com>
2022-11-29 20:45:46 +0100

    added hover to buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./scripts/**/*.{html,js}"
-  ],
+  content: ["./index.html", "./scripts/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        spaceGey: '#343434',
-        sunset: '#FF4E00',
-        tropical: '#00FFDD',
-        glass: '#F5F5F5',
-        rat: '#DBDADA',
-        gravel: '#989898'
-      }
+        spaceGey: "#343434",
+        sunset: "#FF4E00",
+        sunsetHover: "#DD4A09",
+        tropical: "#00FFDD",
+        glass: "#F5F5F5",
+        rat: "#DBDADA",
+        gravel: "#989898",
+      },
     },
   },
   plugins: [],
-}
+};
