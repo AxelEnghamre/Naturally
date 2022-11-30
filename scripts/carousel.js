03835442065ec856e51b0ed042ea1d2aec8174dc
sirisayshello <veckovis.salongen0p@icloud.com>
2022-11-30 20:35:29 +0100

    added video on hover

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -20,7 +20,7 @@ buttonLeft.addEventListener("click", () => {
   });
 });
 
-// hover over product
+// hover over product image/gif
 const productHover = (id, src) => {
   document.getElementById(id).src = src;
 };
@@ -29,23 +29,54 @@ const productStatic = (id, src) => {
   document.getElementById(id).src = src;
 };
 
+// hover over product video
+const setElementDisplay = (id, display) => {
+  document.getElementById(id).style.display = display;
+};
+
 // pants
 const pants = document.querySelector("#pants");
-pants.addEventListener("mouseover", () =>
-  productHover("pants", "assets/images/pants.gif")
-);
-pants.addEventListener("mouseout", () =>
-  productStatic("pants", "assets/images/pants.png")
-);
+const pantsVideo = document.querySelector("#pants-video");
+
+pants.addEventListener("mouseover", () => {
+  setElementDisplay("pants-video", "block");
+  setElementDisplay("pants", "none");
+});
+
+pantsVideo.addEventListener("mouseout", () => {
+  setElementDisplay("pants-video", "none");
+  setElementDisplay("pants", "block");
+});
+
+// const pants = document.querySelector("#pants");
+// pants.addEventListener("mouseover", () =>
+//   productHover("pants", "assets/images/pants.gif")
+// );
+// pants.addEventListener("mouseout", () =>
+//   productStatic("pants", "assets/images/pants.png")
+// );
 
 // t-shirt
 const tshirt = document.querySelector("#t-shirt");
-tshirt.addEventListener("mouseover", () =>
-  productHover("t-shirt", "assets/images/t-shirt.gif")
-);
-tshirt.addEventListener("mouseout", () =>
-  productStatic("t-shirt", "assets/images/t-shirt.png")
-);
+const tshirtVideo = document.querySelector("#t-shirt-video");
+
+tshirt.addEventListener("mouseover", () => {
+  setElementDisplay("t-shirt-video", "block");
+  setElementDisplay("t-shirt", "none");
+});
+
+tshirtVideo.addEventListener("mouseout", () => {
+  setElementDisplay("t-shirt-video", "none");
+  setElementDisplay("t-shirt", "block");
+});
+
+// const tshirt = document.querySelector("#t-shirt");
+// tshirt.addEventListener("mouseover", () =>
+//   productHover("t-shirt", "assets/images/t-shirt.gif")
+// );
+// tshirt.addEventListener("mouseout", () =>
+//   productStatic("t-shirt", "assets/images/t-shirt.png")
+// );
 
 // sunglasses
 const sunglasses = document.querySelector("#sunglasses");
@@ -58,9 +89,22 @@ sunglasses.addEventListener("mouseout", () =>
 
 // balaclava
 const balaclava = document.querySelector("#balaclava");
-balaclava.addEventListener("mouseover", () =>
-  productHover("balaclava", "assets/images/balaclava.gif")
-);
-balaclava.addEventListener("mouseout", () =>
-  productStatic("balaclava", "assets/images/balaclava.png")
-);
+const balaclavaVideo = document.querySelector("#balaclava-video");
+
+balaclava.addEventListener("mouseover", () => {
+  setElementDisplay("balaclava-video", "block");
+  setElementDisplay("balaclava", "none");
+});
+
+balaclavaVideo.addEventListener("mouseout", () => {
+  setElementDisplay("balaclava-video", "none");
+  setElementDisplay("balaclava", "block");
+});
+
+// const balaclava = document.querySelector("#balaclava");
+// balaclava.addEventListener("mouseover", () =>
+//   productHover("balaclava", "assets/images/balaclava.gif")
+// );
+// balaclava.addEventListener("mouseout", () =>
+//   productStatic("balaclava", "assets/images/balaclava.png")
+// );
