1b2feb3b88169c7a154c8d7819505cf553d317ef
Axel Enghamre <axel@enghamre.se>
2022-12-01 21:34:41 +0100

    added english page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./scripts/**/*.{html,js}"],
+  content: ["./index.html","./en/index.html", "./scripts/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
